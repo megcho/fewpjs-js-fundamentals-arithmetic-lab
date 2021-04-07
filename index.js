38b17cdf2da9a2b3ed9342f32e3db967e38d3dcf
megan cho <meganlisacho@gmail.com>
2021-04-07 18:12:03 -0500

    commit tree ddf1f89b91517d6bc9b928933f95c9a22cb3ba26

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+let multiply = num1 * num2;
+console.log(multiply);
+
+
+let random = Math.round(Math.random()*10);
+
+let num3 = 28;
+let num4 = 6;
+
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(1, 3, 5, 20);
